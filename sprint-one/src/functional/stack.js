2d8ad5f06d6d45cc40883c45c8c1b7c0d67a4116
MKS-students <mks.students@hackreactor.com>
2015-07-30 10:48:18 -0500

    stack functional complete

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,17 +1,42 @@
 var Stack = function(){
+  // obejct to return
   var someInstance = {};
 
   // Use an object with numeric keys to store values
+  // do something to fill storage with key val pairs
   var storage = {};
+  var size = 0;
 
   // Implement the methods below
   someInstance.push = function(value){
+    storage[size] = value;
+    size++;
   };
 
   someInstance.pop = function(){
-  };
+    // if (size > 0) {
+    //   // delete storage[size - 1];
+    //   var temp = {};
+    //   for (var key in storage) {
+    //     if (key < size - 1) {
+    //       temp[key] = storage[key];
+    //     }
+    //     return temp;
+    //   }
+    //   storage = temp;
+    //   }
+    //   size--;
+    //   return storage;
+    var pop = storage[size -1]
+    if(size > 0){
+    delete storage[size];
+    size --;
+  }
+  return pop;}
+
 
   someInstance.size = function(){
+    return size;
   };
 
   return someInstance;
