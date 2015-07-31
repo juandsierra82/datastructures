cfd09cdfe0dfdd57cf2a7c9056ce9935ebb3d77b
MKS-students <mks.students@hackreactor.com>
2015-07-31 11:35:21 -0500

    completed linkedlist.could refactor to clean up

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -2,41 +2,45 @@ var LinkedList = function(){
   var list = {};
   list.head = null;
   list.tail = null;
-  list.size = 0;
-  var current;
-  // list.newNode = Node(value);
+;
 
   list.addToTail = function(value){
     // list.added = newNode;
     var newNode = Node(value);
-    if(list.size === 0){ //if theres no nodes in our list
+    if(!list.head){ //if theres no nodes in our list
        list.head = newNode;
        list.tail = list.head;
-       current = newNode
     } else {
-      list.head.next = newNode;
+      list.tail.next = newNode;
       list.tail = newNode;
-      current = list.head;
     }
     console.log(list)
-    list.size++;
   };
 
   list.removeHead = function(){
     // list.head.next = null;
-    var removed = list.head.value;
-    list.head = current.next;
+    var removed = list.head;
+    list.head = removed.next;
     console.log(removed);
     console.log(list);
-    return removed;
+    return removed.value;
   };
 
   list.contains = function(target){
+    var current = list.head;
+    while (current) {
+      if (current.value === target) {
+        return true;
+      } else {
+        current = current.next;
+      }
 
+      }
+            return false;
+    }
+     return list;
   };
 
-  return list;
-};
 
 var Node = function(value){
   var node = {};
