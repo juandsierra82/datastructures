eda439394c6035b48ac409098fc0fdbd58e43ba3
MKS-students <mks.students@hackreactor.com>
2015-07-31 12:42:33 -0500

    contains minus implementation

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,9 +1,11 @@
 var Tree = function(value){
   var newTree = {};
-  newTree.value = value;
+  newTree.value = value; // our root.
 
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];  // fix me
+
+  _.extend(newTree,treeMethods);
 
   return newTree;
 };
@@ -15,12 +17,27 @@ var Tree = function(value){
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
+  var leaf = Tree(value);
+  return this.children.push(leaf);
+
 
 };
 
 treeMethods.contains = function(target){
+   var traverseKids = function (tree){
+     var branches = tree.children;
+   if(branches.length === 0){return false}
+   else{
+     for(var i = 0; i<branches.length; i++){
+       if(branches[i] === target){return true}
+     else{return traverseKids(branches.children[i])}
+   }
+   }
+ }
+ traverseKids(leaf);
+}
 
-};
+
 
 
 /*
