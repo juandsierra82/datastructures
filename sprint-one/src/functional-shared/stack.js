6ed23d25b91d0beb2f6feb27d2b0795485c68b69
MKS-students <mks.students@hackreactor.com>
2015-07-30 19:58:53 -0500

    stack completed for functional shared

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -2,61 +2,42 @@ var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   // // var obj = Stack.bind(stackMethods);
-    var stack = {};
-// USE UNDERSCORE EXTEND!!!
+    var stack = {
 
-stack.storage = {};
-stack.size = 0;
+}
 
-  // var extend = function (object) {
-  //   var len = arguments.length;
-  //   for (var i = 0; i < len; i++) {
-  //     for (var prop in arguments[i]) {
-  //       object[prop] = arguments[i][prop];
-  //     }
-  //   }
-  //   return object;
-  // }
-
-
-
-
-
-// stack.push = stackMethods.push;
-// stack.pop = stackMethods.pop;
-// stack.size = stackMethods.size;
-
-stack = extend(stack, stackMethods);
+    stack.storage = {};
+    stack.theSize = 0;
+  _.extend(stack, stackMethods);
 
 return stack;
 
-
-
-
 };
 
-var stackMethods =  {
+var stackMethods =  {};
+
+    stackMethods.push = function(value){
+    this.storage[this.theSize] = value;
+    this.theSize++;
+    };
+
+  stackMethods.pop = function(){
+
+    var pop = this.storage[this.theSize -1]
+    if(this.theSize > 0){
+    delete this.storage[this.theSize];
+    this.theSize --;
+    }
+    return pop;
+  };
+
+
+  stackMethods.size = function(){
+  return this.theSize;
+  };
 
 
 
-  push : function(value){
-    this.storage[this.size] = value;
-    this.size++;
-  },
 
-  pop : function(){
-
-    var pop = this.storage[this.size -1]
-    if(this.size > 0){
-    delete this.storage[this.size];
-    this.size --;
-  }
-  return pop;},
-
-
-  size : function(){
-    return this.size;
-  }
-};
 
 
