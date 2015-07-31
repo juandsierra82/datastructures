d4cf7b2648b8732936795d2e6be00f7145230e5c
MKS-students <mks.students@hackreactor.com>
2015-07-31 11:09:49 -0500

    up until remove head; error: should contain value added

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -2,14 +2,37 @@ var LinkedList = function(){
   var list = {};
   list.head = null;
   list.tail = null;
+  list.size = 0;
+  var current;
+  // list.newNode = Node(value);
 
   list.addToTail = function(value){
+    // list.added = newNode;
+    var newNode = Node(value);
+    if(list.size === 0){ //if theres no nodes in our list
+       list.head = newNode;
+       list.tail = list.head;
+       current = newNode
+    } else {
+      list.head.next = newNode;
+      list.tail = newNode;
+      current = list.head;
+    }
+    console.log(list)
+    list.size++;
   };
 
   list.removeHead = function(){
+    // list.head.next = null;
+    var removed = list.head.value;
+    list.head = current.next;
+    console.log(removed);
+    console.log(list);
+    return removed;
   };
 
   list.contains = function(target){
+
   };
 
   return list;
@@ -20,6 +43,7 @@ var Node = function(value){
 
   node.value = value;
   node.next = null;
+  node.previous = null;
 
   return node;
 };
