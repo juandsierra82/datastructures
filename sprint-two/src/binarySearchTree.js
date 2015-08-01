16ff8349638960046e66fffe3530337bcc8f92a9
MKS-students <mks.students@hackreactor.com>
2015-08-01 16:31:54 -0500

    binary dearch tree complete

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,34 +1,46 @@
-var BinarySearchTree = function(value){
-var newTree = Object.create(binaryMethods);
-newTree.right = null;
-newTree.left = null;
-newTree.value = value;
 
-return newTree;
+var BinarySearchTree = function(value) {
+  var newTree = Object.create(binaryMethods);
+  newTree.right = null;
+  newTree.left = null;
+  newTree.value = value;
+  return newTree;
 }
-
 var binaryMethods = {
-
-  insert : function(value){
-    if(value)
-    this.value = BinarySearchTree(value);
-
+  insert: function(val) {
+    if (val === this.value) {
+      return
+    } else if (val > this.value) {
+      if (this.right !== null) {
+        this.right.insert(val);
+      } else {
+        this.right = BinarySearchTree(val);
+      }
+    } else if (val < this.value) {
+      if (this.left !== null) {
+        this.left.insert(val);
+      } else {
+        this.left = BinarySearchTree(val);
+      }
+    }
   },
-  contains : function(value){
-
+  contains: function(val) {
+    if (this.value === val) {
+      return true;
+    } else if (this.value < val && this.right !== null) {
+      return this.right.contains(val);
+    } else if (this.value > val && this.left !== null) {
+      return this.left.contains(val);
+    }
+    return false;
   },
-  depthFirstLog: function(value){
-
+  depthFirstLog: function(cb) {
+    cb(this.value);
+    if (!!this.left) {
+      this.left.depthFirstLog(cb);
+    }
+    if (!!this.right) {
+      this.right.depthFirstLog(cb);
+    }
   }
 }
-
-var binaryTree = BinarySearchTree(5);
-binaryTree.insert(3);
-binaryTree.insert(6);
-console.log(binaryTree);
-
-
-
-/*
- * Complexity: What is the time complexity of the above functions?
- */
