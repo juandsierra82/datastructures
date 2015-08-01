3a1e1ffe6b2cc77752ef044d2a29a40e140ce2be
MKS-students <mks.students@hackreactor.com>
2015-08-01 13:58:33 -0500

    graph changed

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -1,19 +1,13 @@
 
 var Graph = function(){
-  // var index = 0;
-  // this.index = 0;
+
 this.nodes = {};
-  // this.nodes.bucket = [];
-  // this.nodes[this.index] = [];
-  // this.nodes.node = {};
-  this.edges = {};
-  this.index = 0;
-  this.edges.names = [];
+
 };
 
 Graph.prototype.addNode = function(node){
 
-this.nodes[node] = node;
+this.nodes[node] = [];
 
 };
 
@@ -21,49 +15,37 @@ this.nodes[node] = node;
 
 
 Graph.prototype.contains = function(node){
-  return _.contains(this.nodes, node);
+  if (this.nodes[node]) return true;
+
+return false;
 }
 
 Graph.prototype.removeNode = function(node){
-  if (_.contains(this.nodes, node)) {
-    delete this.nodes[node];
-    for(var i = 0; i<this.edges.names.length; i++){
-      if(this.edges.names[i].indexOf(node) > -1) {
-        delete this.edges.names[i];
-      }
-    }
-  }
+  delete this.nodes[node];
 };
 
 Graph.prototype.hasEdge = function(fromNode, toNode){
-  var args = Array.prototype.slice.call(arguments)
-  var target = args.join(',')
-  return _.contains(this.edges.names, target);
+  return _.contains(this.nodes[fromNode], toNode)
+
 };
 
 Graph.prototype.addEdge = function(fromNode, toNode){
-   this.edges[this.index] = [];
-
-  if (this.nodes[fromNode] && this.nodes[toNode] && fromNode !== toNode) {
-    this.edges[this.index].push(fromNode);
-    this.edges[this.index].push(toNode);
-  }
-this.edges.names.push(this.edges[this.index].join(','));
-
-  this.index++;
+  this.nodes[fromNode].push(toNode);
+  this.nodes[toNode].push(fromNode);
 }
 
 Graph.prototype.removeEdge = function(fromNode, toNode){
-  for (var i = 0; i < this.edges.names.length; i++) {
-    if(this.edges.names[i].indexOf(fromNode) > -1 && this.edges.names[i].indexOf(toNode) > -1) {
-      delete this.edges.names[i];
-      delete this.edges[i];
+  for (var i = 0; i<this.nodes[fromNode].length; i++){
+    if(this.nodes[fromNode][i] === toNode){
+      this.nodes[fromNode].splice(i, 1);
     }
   }
 };
 
 Graph.prototype.forEachNode = function(cb){
-  return _.map(this.nodes, cb);
+  return _.each(this.nodes, function(el, i){
+    cb(i);
+  })
 };
 
 /*
